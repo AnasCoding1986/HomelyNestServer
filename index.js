666a62af47d7993ef072b3e8abfd641097f94cf9
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-11-18 18:38:14 +0600

    lets see what happen next

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ async function run() {
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
           })
           .send({ success: true })
-        console.log('Logout successful would omit')
+        console.log('Logout successful would omit but need now lets see what happen next')
       } catch (err) {
         res.status(500).send(err)
       }
